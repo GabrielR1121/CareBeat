2ffacfc4bf54b9add06d2bb06c0306165a54de2e
Gabriel Rodriguez <gr.garcia1121@gmail.com>
2023-11-23 13:08:08 -0400

    Edited and Commented init.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_session import Session
 from flask_login import LoginManager
-from website.config import db
-
+from website.config import db, config
 
 
 def create_app():
@@ -10,8 +9,8 @@ def create_app():
     This is the starting method of the flask application. All the flask BASIC configurations are done here
     '''
     app = Flask(__name__)
-    #This secret key will eventually change so its not visible to everyone on github
-    app.config['SECRET_KEY'] = 'QWERTY'
+
+    
 
     #Imports the views file and registers the routes inside the application
     from .views import views
@@ -20,16 +19,22 @@ def create_app():
     app.register_blueprint(views,url_prefix="/")
     app.register_blueprint(auth,url_prefix="/")
 
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_KEY_PREFIX'] = 'your_session_prefix'
+    #Creates a secure key to store the session under
+    app.config['SECRET_KEY'] = config.SECRET_KEY
+
+    #Session configurations to make sure session data is handled correctly
+    app.config['SESSION_TYPE'] = config.SESSION_TYPE
+    app.config['SESSION_PERMANENT'] = config.SESSION_PERMANENT
+    app.config['SESSION_USE_SIGNER'] = config.SESSION_USE_SIGNER
+    app.config['SESSION_KEY_PREFIX'] = config.SESSION_KEY_PREFIX
     Session(app)
 
+    #Creates an instance of the flask login to pass to the application
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    #After the user is autheticated the user is loaded to the flask session with all of their info
     @login_manager.user_loader
     def load_user(id):
         user_data = db.get_caretaker(id)
